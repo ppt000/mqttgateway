499d90213d81bf18f618bd76d92971fc66cc0e2a
ppt000 <paolo.taddonio@empiluma.com>
2018-05-30 15:44:10 +0100

    include initialisation of Properties
    as described in the docstring, the whole module needs the Properties to be initialised. If the calling application has not done it, it is done here.

--- mqttgateway/gateway/__init__.py
+++ mqttgateway/gateway/__init__.py
@@ -1,4 +1,6 @@
 '''
+.. reviewed 29May2018
+
 The package representing the *core* of the application.
 
 There are 4 modules:
@@ -10,4 +12,17 @@ There are 4 modules:
 - :mod:`start_gateway.py` which contains the script for the
   application initialisation and main loop.
 - :mod:`configuration.py` which contains the default configuration as a string.
+
+
+This package uses the logger help provided by :mod:`mqttgateway.utils.app_properties`.
+The :data:`Properties` object should have been already initialised by the application
+using this library.
+However, if that is not the case, the initialisation is done here with default parameters.
+As a consequence, the main application should initialise :data:`Properties` before
+importing anything from this package.
 '''
+
+
+import os.path
+import mqttgateway.utils.app_properties as app
+app.Properties.init(app_path=os.path.realpath(__file__), app_name='mqttgateway')
