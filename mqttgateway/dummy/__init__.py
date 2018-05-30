eb2fcfadcfbe6b6c53fedf62aace8e9612c76d36
ppt000 <paolo.taddonio@empiluma.com>
2018-05-30 15:40:27 +0100

    format

--- mqttgateway/dummy/__init__.py
+++ mqttgateway/dummy/__init__.py
@@ -2,8 +2,5 @@
 A dummy gateway to test the installation setup, the loading of the
 configuration files, and the basic operation of the core application.
 
-There are 2 modules:
-
-- :mod:`dummy_interface.py` defines the class :class:`dummyInterface`;
-- :mod:`dummy2mqtt.py` which is the application launcher.
+.. reviewed 30 May 2018
 '''
