1cb45e0b1cfa053b01b6180a4c7b7742dcdfff09
ppt000 <paolo.taddonio@empiluma.com>
2018-05-30 15:43:04 +0100

    update docstrings

--- mqttgateway/entry/entry_start.py
+++ mqttgateway/entry/entry_start.py
@@ -1,5 +1,8 @@
 '''
 Launcher script for the **entry** gateway.
+
+.. reviewed 30 May 2018
+
 '''
 
 import os.path
@@ -12,11 +15,11 @@ app.Properties.init(os.path.realpath(__file__), app_name=_APP_NAME)
 # import the module that initiates and starts the gateway
 import mqttgateway.gateway.start_gateway as start_g
 
-# import the module representing the interface *** change to your import here ***
+# import the module representing the interface
 import mqttgateway.entry.entry_interface as entry_i
 
 def main():
-    # launch the gateway *** change to your class here ***
+    ''' launch the gateway'''
     start_g.startgateway(entry_i.entryInterface)
 
 if __name__ == '__main__':
