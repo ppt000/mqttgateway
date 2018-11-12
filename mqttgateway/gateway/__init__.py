3111fff8873cdb8b2c5d418b5a7b59133b508137
ppt000 <paolo.taddonio@empiluma.com>
2018-11-12 08:29:19 +0000

    Update package information

--- mqttgateway/gateway/__init__.py
+++ mqttgateway/gateway/__init__.py
@@ -5,10 +5,9 @@ The package representing the *core* of the application.
 
 There are 4 modules:
 
-- :mod:`mqtt_client.py` defines the child class of the official MQTT
+- :py:mod:`mqtt_client` defines the child class of the official MQTT
   Client class of the paho library;
-- :mod:`mqtt_map.py` defines the classes :class:`internalMsg` and
-  :class:`msgMap`;
+- :mod:`mqtt_map.py` defines the classes :class:`internalMsg` and :class:`msgMap`;
 - :mod:`start_gateway.py` which contains the script for the
   application initialisation and main loop.
 - :mod:`configuration.py` which contains the default configuration as a string.
