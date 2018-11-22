009c9784868f8a352a270d108ab67fe28422181f
Paolo Taddonio <paolo.taddonio@empiluma.com>
2018-11-22 12:19:55 +0000

    Update mqtt_map.py
    Change in str method for internal messages.
    Remove the test  (moved in new test module).

--- mqttgateway/mqtt_map.py
+++ mqttgateway/mqtt_map.py
@@ -76,16 +76,16 @@ class internalMsg(object):
                            action=self.action,
                            arguments=self.arguments.copy())
 
-    def str(self):
+    def __str__(self):
         ''' Stringifies the instance content.'''
-        return ''.join(('type= ', 'C' if self.iscmd else 'S',
-                        '- function= ', str(self.function),
-                        '- gateway= ', str(self.gateway),
-                        '- location= ', str(self.location),
-                        '- device= ', str(self.device),
-                        '- sender= ', str(self.sender),
-                        '- action= ', str(self.action),
-                        '- arguments= ', str(self.arguments)
+        return ' - '.join(('type=' + 'C' if self.iscmd else 'S',
+                        'function=' + str(self.function),
+                        'gateway=' + str(self.gateway),
+                        'location=' + str(self.location),
+                        'device=' + str(self.device),
+                        'sender=' + str(self.sender),
+                        'action=' + str(self.action),
+                        'arguments=' + str(self.arguments)
                        ))
 
     def reply(self, response, reason):
@@ -411,43 +411,5 @@ class msgMap(object):
         mqtt_msg.retain = False
         return mqtt_msg
 
-def test():
-    ''' Test function. '''
-    # load a valid map in JSON format
-    jsonfilepath = './test_map2.json'
-    with open(jsonfilepath, 'r') as json_file:
-        json_data = json.load(json_file)
-    # instantiate a class
-    msgmap = msgMap(json_data)
-    # printout some members
-    function = msgmap.maps.function.m2i('lighting'); print function
-    gateway = msgmap.maps.gateway.m2i('dummy'); print gateway
-    location = msgmap.maps.location.m2i('office'); print location
-    device = msgmap.maps.device.m2i('kitchen_track'); print device
-    sender = msgmap.maps.sender.m2i('me'); print sender
-    action = msgmap.maps.action.m2i('light_on'); print action
-    m_args = {'key1': 'value1'}
-    i_args = {}
-    for key, value in m_args.iteritems():
-        i_args[msgmap.maps.argkey.m2i(key)] = msgmap.maps.argvalue.m2i(value)
-    print i_args
-
-def reverse():
-    ''' Another test function.'''
-    jsonfilepath = './test_map.json'
-    with open(jsonfilepath, 'r') as json_file:
-        json_data = json.load(json_file)
-    for item in ('function', 'gateway', 'location', 'device', 'sender', 'action', 'argkey', 'argvalue'):
-        if 'map' not in json_data[item]: continue
-        newmap = {}
-        oldmap = json_data[item]['map']
-        for key, value in oldmap.iteritems():
-            newmap[value] = []
-            newmap[value].append(key)
-        json_data[item]['map'] = newmap
-    print json.dumps(json_data)
-    return
-
 if __name__ == '__main__':
-    test()
-    #reverse()
+    pass
