eaca7f731940ce94f0e7f8d96c1b259b725c8a63
Paolo Taddonio <paolo.taddonio@empiluma.com>
2018-11-27 07:43:03 +0000

    New version and other py3 related constants

--- mqttgateway/__init__.py
+++ mqttgateway/__init__.py
@@ -45,5 +45,10 @@ This package has 4 *groups* of files:
 
 '''
 
-__version_info__ = (1, 1, 1)
+import sys
+PY2 = sys.version_info[0]==2
+
+ENCODING = 'utf-8'
+
+__version_info__ = (1, 1, 2)
 __version__ = '.'.join(str(c) for c in __version_info__)
