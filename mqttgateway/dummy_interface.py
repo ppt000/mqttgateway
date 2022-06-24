190b8d980b864b58251449700b13328ee540b8d3
Paolo Taddonio <paolo.taddonio@empiluma.com>
2022-06-24 14:50:37 +0100

    Clean-up of example case after rewrite

--- mqttgateway/dummy_interface.py
+++ mqttgateway/dummy_interface.py
@@ -1,28 +1,27 @@
 ''' The **dummy** interface class definition. Use it as a template.
 
-.. REVIEWED 11 November 2018
+.. REVIEWED 20jun22
 
 This module defines the class :py:class:`dummyInterface` that will be instantiated by the
 module :py:mod:`~mqttgateway.start_gateway`.
 '''
 
 import logging
-
-# module time is imported because of the example code in loop()
 import time
 
-import mqttgateway.mqtt_map as mqtt_map
+from mqttgateway import mqtt_map
 
 LOG = logging.getLogger(__name__)
 
-class dummyInterface(object):
+class dummyInterface:
     ''' An interface that doesn't do anything but allows to test the installation.
 
     The minimum requirement for the interface class is to define 2 out of 3 possible public
     methods:
 
     - the constructor ``__init__``,
-    - either the ``loop`` method or the ``loop_start`` method.
+    - either the ``loop`` method or the ``loop_start`` method,
+    - a ``loop_stop`` method if a ``loop_start`` is defined.
 
     Args:
         params (dictionary of strings): contains all the options from the configuration file
@@ -31,23 +30,23 @@ class dummyInterface(object):
             entry in the dictionary. Use this to pass parameters from the configuration
             file to the interface, for example the name of a port, or the speed
             of a serial communication.
-        msglist_in (:py:class:`~mqttgateway.mqtt_map.MsgList` object): list of incoming messages
+        msglist_in (:py:class:`~mqttgateway.mqtt_map.MsgList`): list of incoming messages
             in their internal representation.
-        msglist_out (:py:class:`~mqttgateway.mqtt_map.MsgList` object): list of outgoing messages
+        msglist_out (:py:class:`~mqttgateway.mqtt_map.MsgList`): list of outgoing messages
             in their internal representation.
     '''
 
-    def __init__(self, params, msglist_in, msglist_out):
+    def __init__(self, params: dict, msglist_in: mqtt_map.MsgList, msglist_out: mqtt_map.MsgList):
         # optional welcome message
-        LOG.debug(''.join(('Module <', __name__, '> started.')))
+        LOG.debug('Module <%s> started.', __name__)
         # example of how to use the 'params' dictionary
         try: port = params['port'] # the 'port' option should be defined in the configuration file
-        except KeyError: # if it is not, we are toast, or a default could be provided
+        except KeyError as err: # if it is not, we are toast, or a default could be provided
             errormsg = 'The "port" option is not defined in the configuration file.'
-            LOG.critical(''.join(('Module ', __name__, ' could not start.\n', errormsg)))
-            raise KeyError(errormsg)
+            LOG.critical('Module <%s> could not start.', __name__)
+            raise RuntimeError(errormsg) from err
         # optional success message
-        LOG.debug(''.join(('Parameter "port" successfully updated with value <', port, '>')))
+        LOG.debug('Parameter "port" successfully updated with value <%s>', port)
         # *** INITIALISE YOUR INTERFACE HERE ***
 
         # Keep the message lists locally
@@ -67,7 +66,7 @@ class dummyInterface(object):
             msg = self._msgl_in.pull()
             if msg is None: break
             # do something with the message; here we log only
-            LOG.debug(''.join(('Internal message received:\n\t', str(msg))))
+            LOG.debug('Internal message received: %s', msg)
         # example code to write in the outgoing messages list periodically
         timenow = time.time()
         if (timenow - self.time0) > 30: # every 30 seconds
